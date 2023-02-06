7a7c5d9b656d9fb8a6d4be21996c488e1b759665
mouni025 <im.mounika025@gmail.com>
2023-02-06 17:13:51 +0530

    added packages

--- App.js
+++ App.js
@@ -1,3 +1,33 @@
+/*
+ *** Parcel Feature ***
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev abd Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev
+ * port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Tree shaking
+ *
+ *
+ * Transitive Dependencies
+ */
+
+
+
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading=React.createElement(
     "h1",
     {id:"heading"},
