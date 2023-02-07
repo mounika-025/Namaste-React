18c6890996cd7aa337ac4d862a4899c71451e522
mouni025 <im.mounika025@gmail.com>
2023-02-07 22:39:43 +0530

    added chapter 3

---
+++ Chapter-3 Laying the Foundation/App.js
@@ -0,0 +1,47 @@
+
+
+
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+//JSX => React.createElement => Object =>HTML(DOM)
+
+const Heading=()=>(
+   <div>
+        <h1 id="title" key="h2">Hello</h1> 
+       
+   </div>
+
+   //Jsx expression
+    
+)
+
+// React Components
+//- Functional component =>New way
+
+const HeaderComponent=()=>{      //A normal function that returns jsx
+
+   return(
+       <div>
+       <Heading/>      
+       <h2>Functional Component</h2> 
+       </div> 
+
+   ) 
+
+}
+
+
+//-Class component=> Old way
+
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+// async defer
+root.render(<HeaderComponent/>)
+
+
