c8e55305a86d113364704a7d2c84426ce30b5980
mouni025 <im.mounika025@gmail.com>
2023-03-10 22:56:15 +0530

    minor changes

--- Chapter-7 Finding the Path/Components/RestaurantMenu.js
+++ Chapter-7 Finding the Path/Components/RestaurantMenu.js
@@ -27,13 +27,22 @@ const RestaurantMenu=()=>{
     };
 
     const menu =(restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards)
+    console.log(menu)
     
     
     return (!restaurant)? <Shimmer/>:(
         <div className="restaurant-menu">
             <div className="restaurant-description">
+            
             <img className="restaurant-image" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+restaurant?.cards[0]?.card?.card?.info?.cloudinaryImageId}/>
-            <h1>{restaurant?.cards[0]?.card?.card?.info?.name}</h1>
+            
+            <div className="restaurant-description-details">
+            <h1 className="restaurant-name">{restaurant?.cards[0]?.card?.card?.info?.name}</h1>
+            <p className="restaurant-cuisine">{restaurant?.cards[0]?.card?.card?.info?.cuisines.join(", ")}</p>
+            <p className="restaurant-area">{restaurant?.cards[0]?.card?.card?.info?.locality}</p>
+            <i className="fa-sharp fa-solid fa-clock"></i>
+            </div>
+            
             </div>
             <div>
                 <h1>Menu</h1>
